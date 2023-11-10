be23fc909682edee9331db23b2a6e512017312c2
HYUNJUN\sajun <sajun28@naver.com>
2023-11-10 16:38:14 +0900

    Feat: "예외 발생시 다시 시작 기능 추가"

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,17 +2,24 @@ package christmas;
 
 import christmas.model.ValidateDate;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class Application {
 
     private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     public static void validateDate() {
-        new ValidateDate(inputView.readDate());
+        try {
+            new ValidateDate(inputView.readDate());
+        } catch (IllegalArgumentException e) {
+            outputView.errorMessage(e.getMessage());
+            validateDate();
+        }
     }
 
-
     public static void main(String[] args) {
+        outputView.startMessage();
         validateDate();
     }
 }
