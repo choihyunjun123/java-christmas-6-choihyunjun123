3b4e13ce5f66402b98ff3325772c0b9147fba64e
HYUNJUN\sajun <sajun28@naver.com>
2023-11-10 16:00:37 +0900

    Feat: "방문 날짜 입력 기능 추가"

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.view.InputView;
+
 public class Application {
+
+    private static final InputView inputView = new InputView();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        inputView.readDate();
     }
 }
