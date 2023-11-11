5935efdae35a148046023cbdfcd2f98d84ad3a55
HYUNJUN\sajun <sajun28@naver.com>
2023-11-12 02:51:39 +0900

    Refactor: "출력 위치 변경"

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,6 +12,7 @@ public class OutputView {
 
     public void preview(int day) {
         System.out.println("12월 " + day + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+        System.out.println("<주문 메뉴>");
     }
 
     public void orderList(String key, Integer value) {
