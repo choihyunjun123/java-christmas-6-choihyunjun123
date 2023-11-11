54833f1f4a1e6ced01e49a1a8282636228a38248
HYUNJUN\sajun <sajun28@naver.com>
2023-11-12 00:35:51 +0900

    Refactor: "상수 변수화"

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -7,11 +7,16 @@ import java.util.List;
 
 public class Order {
 
+    private static final int MENU_NAME_POSITION = 0;
+    private static final int MENU_NUMBER_POSITION = 1;
+    private static final String MENU_SPLIT_PUNCTUATION = ",";
+    private static final String NUMBER_SPLIT_PUNCTUATION = "-";
+
     public List<List> splitOrder(String order) {
-        List<String> orders = Arrays.asList(order.split(","));
+        List<String> orders = Arrays.asList(order.split(MENU_SPLIT_PUNCTUATION));
         List<List> splitOrders = new ArrayList<>();
         for (String s : orders) {
-            splitOrders.add(Arrays.asList(s.split("-")));
+            splitOrders.add(Arrays.asList(s.split(NUMBER_SPLIT_PUNCTUATION)));
         }
         return splitOrders;
     }
@@ -19,7 +24,7 @@ public class Order {
     public HashMap<Object,Object> putOrder(List<List> order) {
         HashMap<Object,Object> menuAndNumber = new HashMap<>();
         for (List l : order) {
-            menuAndNumber.put(l.get(0),l.get(1));
+            menuAndNumber.put(l.get(MENU_NAME_POSITION),l.get(MENU_NUMBER_POSITION));
         }
         return menuAndNumber;
     }
