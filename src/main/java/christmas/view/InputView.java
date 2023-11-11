06fda0e056501c528c4df4c1f28d03045954ba89
HYUNJUN\sajun <sajun28@naver.com>
2023-11-11 21:39:13 +0900

    Refactor: "띄어쓰기 수정"

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -10,7 +10,7 @@ public class InputView {
     }
 
     public String readMenu() {
-        System.out.println("\n주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Console.readLine();
     }
 }
