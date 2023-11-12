62af10f97acab9aa16440001eab9259d7b6e0626
HYUNJUN\sajun <sajun28@naver.com>
2023-11-12 21:10:08 +0900

    Refactor: "상수의 변수화"

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -12,8 +12,10 @@ public class Discount {
     private static final int CHRISTMAS_DISCOUNT_DUE_DATE = 25;
     private static final int CHRISTMAS_DISCOUNT_START_PRICE = 1000;
     private static final int CHRISTMAS_DISCOUNT_INCREMENT = 100;
-
-    private static final OutputView outputView = new OutputView();
+    private static final int EVENT_YEAR = 2023;
+    private static final int EVENT_MONTH = 12;
+    private static final int WEEKDAY_DISCOUNT_INCREMENT = 2023;
+    
     private static HashMap<String, Integer> discoutAndNumbers = new HashMap<>();
 
     public HashMap<String, Integer> totalDiscount(int visitDay, HashMap<String, Integer> menuAndNumber) {
@@ -36,9 +38,9 @@ public class Discount {
     public int weekday(int visitDay, HashMap<String, Integer> menuAndNumber) {
         int weekdayDiscount = INITIAL_VALUE_OF_DISCOUNT;
         int dessertNum = new Validate().menuCheck(menuAndNumber, Menu.Dessert.values());
-        LocalDate day = LocalDate.of(2023, 12, visitDay);
+        LocalDate day = LocalDate.of(EVENT_YEAR, EVENT_MONTH, visitDay);
         if (!day.getDayOfWeek().toString().equals("FRIDAY") || !day.getDayOfWeek().toString().equals("SATURDAY")) {
-            weekdayDiscount = 2023 * dessertNum;
+            weekdayDiscount = WEEKDAY_DISCOUNT_INCREMENT * dessertNum;
         }
         if (weekdayDiscount != INITIAL_VALUE_OF_DISCOUNT) {
             discoutAndNumbers.put("평일 할인", weekdayDiscount);
