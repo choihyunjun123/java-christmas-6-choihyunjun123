89729c6d8bcb27942c370d151abc3132ab77c052
HYUNJUN\sajun <sajun28@naver.com>
2023-11-14 13:56:44 +0900

    Test: "테스트 예제 변경"

--- src/test/java/christmas/model/PaymentTest.java
+++ src/test/java/christmas/model/PaymentTest.java
@@ -18,9 +18,9 @@ class PaymentTest {
     @BeforeAll
     static void beforeAll() {
         menuAndNumber.put("양송이스프", 1);
-        menuAndNumber.put("티본스테이크", 2);
-        menuAndNumber.put("초코케이크", 3);
-        menuAndNumber.put("제로콜라", 4);
+        menuAndNumber.put("티본스테이크", 1);
+        menuAndNumber.put("초코케이크", 1);
+        menuAndNumber.put("제로콜라", 1);
     }
 
     @DisplayName("할인 전 주문 금액")
@@ -29,7 +29,7 @@ class PaymentTest {
         int result = 0;
         Payment payment = new Payment();
         result = payment.originalAmount(menuAndNumber);
-        assertThat(result).isEqualTo(173000);
+        assertThat(result).isEqualTo(79000);
     }
 
     @DisplayName("에피타이저 주문 금액")
@@ -51,7 +51,7 @@ class PaymentTest {
         for (Map.Entry<String, Integer> entry : menuAndNumber.entrySet()) {
             result += payment.mainAmount(entry);
         }
-        assertThat(result).isEqualTo(110000);
+        assertThat(result).isEqualTo(55000);
     }
 
     @DisplayName("디저트 주문 금액")
@@ -62,7 +62,7 @@ class PaymentTest {
         for (Map.Entry<String, Integer> entry : menuAndNumber.entrySet()) {
             result += payment.dessertAmount(entry);
         }
-        assertThat(result).isEqualTo(45000);
+        assertThat(result).isEqualTo(15000);
     }
 
     @DisplayName("음료 주문 금액")
@@ -73,6 +73,29 @@ class PaymentTest {
         for (Map.Entry<String, Integer> entry : menuAndNumber.entrySet()) {
             result += payment.drinkAmount(entry);
         }
-        assertThat(result).isEqualTo(12000);
+        assertThat(result).isEqualTo(3000);
+    }
+
+    @DisplayName("메뉴 주문 개수 계산")
+    @Test
+    void menuCheck() {
+        Payment payment = new Payment();
+        HashMap<String, Integer> menuAndNumber = new HashMap<>();
+        menuAndNumber.put("양송이스프", 1);
+        menuAndNumber.put("타파스", 1);
+        int result = payment.menuCheck(menuAndNumber, Menu.Appetizer.values());
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void menuOrderNum() {
+        Payment payment = new Payment();
+        HashMap<String, Integer> menuAndNumber = new HashMap<>();
+        menuAndNumber.put("양송이스프", 1);
+        menuAndNumber.put("티본스테이크", 2);
+        menuAndNumber.put("초코케이크", 3);
+        menuAndNumber.put("제로콜라", 4);
+        int result = payment.menuOrderNum(menuAndNumber);
+        assertThat(result).isEqualTo(10);
     }
 }
