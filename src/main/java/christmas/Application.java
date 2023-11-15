801ed51a01eab236386dacfa9b266938f0f8497d
HYUNJUN\sajun <sajun28@naver.com>
2023-11-15 16:15:38 +0900

    Refactor: "함수 수정"

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -26,8 +26,19 @@ public class Application {
     private static HashMap<String, Integer> menuAndNumber = new HashMap<>();
     private static HashMap<String, Integer> discountAndAmount = new HashMap<>();
 
-    //입력 날짜 검증
-    public static void validateDate() {
+    public static void main(String[] args) {
+        outputView.start();
+        validateDate();
+        showOrder(menuAndNumber);
+        showOriginalPrice();
+        showGift(originalPrice);
+        showDiscount(discount.totalDiscount(visitDay, originalPrice, menuAndNumber));
+        showTotalDiscount(discountAndAmount);
+        showPayAmount(discountAndAmount);
+        showBadge(discountAndAmount);
+    }
+
+    private static void validateDate() {
         try {
             visitDay = validate.validateDate(inputView.readDate());
             validateMenu();
@@ -38,8 +49,7 @@ public class Application {
         }
     }
 
-    //입력 메뉴 검증
-    public static void validateMenu() {
+    private static void validateMenu() {
         try {
             menuAndNumber = validate.validateOrder(inputView.readMenu());
         } catch (IllegalArgumentException e) {
@@ -48,93 +58,66 @@ public class Application {
         }
     }
 
-    //주문 메뉴 출력
-    public static void showOrder(HashMap<String, Integer> orders) {
-        for (Map.Entry<String, Integer> entry : orders.entrySet()) {
-            outputView.orderList(entry.getKey(), entry.getValue());
-        }
+    private static void showOrder(HashMap<String, Integer> orders) {
+        orders.forEach(outputView::orderList);
     }
 
-    //할인 전 총주문 금액 출력
-    public static void showOriginalPrice() {
+    private static void showOriginalPrice() {
         originalPrice = new Payment().originalAmount(menuAndNumber);
         outputView.originalPrice(won.format(originalPrice));
     }
 
-    //증정 매뉴 출력
-    public static void showGift(int originalPrice) {
+    private static void showGift(int originalPrice) {
         outputView.giftStart();
         if (discount.giftPut(originalPrice)) {
             outputView.gift();
-        }
-        if (!discount.giftPut(originalPrice)) {
+        } else {
             outputView.none();
         }
     }
 
-    //할인 목록 출력
-    public static void showDiscount(HashMap<String, Integer> discountTotal) {
+    private static void showDiscount(HashMap<String, Integer> discountTotal) {
         if (originalPrice >= PRICE_OF_EVENT_START) {
             discountAndAmount = discountTotal;
         }
         discountList(discountAndAmount);
     }
 
-    //할인 목록 표시
-    public static void discountList(HashMap<String, Integer> discount) {
+    private static void discountList(HashMap<String, Integer> discount) {
         outputView.discountStart();
         if (!discount.isEmpty()) {
-            for (Map.Entry<String, Integer> entry : discount.entrySet()) {
-                outputView.discountList(entry.getKey(), won.format(entry.getValue()));
-            }
-        }
-        if (discount.isEmpty()) {
+            discount.forEach((discountName, discountAmount) ->
+                    outputView.discountList(discountName, won.format(discountAmount)));
+        } else {
             outputView.none();
         }
     }
 
-    //총 혜택 금액 출력
-    public static void showTotalDiscount(HashMap<String, Integer> discountTotal) {
+    private static void showTotalDiscount(HashMap<String, Integer> discountTotal) {
         outputView.discountTotalStart();
         int totalDiscount = discount.totalDiscountAmount(discountTotal);
         if (discountTotal.isEmpty()) {
             outputView.none();
-        }
-        if (!discountTotal.isEmpty()) {
+        } else {
             outputView.discountTotal(won.format(totalDiscount));
         }
     }
 
-    //최종 결제 금액 출력
-    public static void showPayAmount(HashMap<String, Integer> discountTotal) {
+    private static void showPayAmount(HashMap<String, Integer> discountTotal) {
         outputView.payAmountStart();
         int totalDiscount = discount.totalDiscountAmount(discountTotal);
         int finalPrice = discount.fianlPayAmount(originalPrice, totalDiscount);
         if (discountTotal.isEmpty()) {
             outputView.payAmount(won.format(originalPrice));
-        }
-        if (!discountTotal.isEmpty()) {
+        } else {
             outputView.payAmount(won.format(finalPrice));
         }
     }
 
-    //배지 출력
-    public static void showBadge(HashMap<String, Integer> discountTotal) {
+    private static void showBadge(HashMap<String, Integer> discountTotal) {
         EventBadge badge = new EventBadge();
         int totalDiscount = discount.totalDiscountAmount(discountTotal);
         outputView.badgeStart();
         outputView.badge(badge.presentBadge(totalDiscount));
-    }
-
-    public static void main(String[] args) {
-        outputView.start(); // 주문 시작
-        validateDate(); // 주문 날짜 검증
-        showOrder(menuAndNumber); // 주문 목록 출력
-        showOriginalPrice();  // 할인 전 총주문 금액 출력
-        showGift(originalPrice); // 증정 메뉴 출력
-        showDiscount(discount.totalDiscount(visitDay, originalPrice, menuAndNumber)); // 혜택 내역 출력
-        showTotalDiscount(discountAndAmount); // 총 혜택 금액 출력
-        showPayAmount(discountAndAmount); // 할인 후 예상 결제 금액 출력
-        showBadge(discountAndAmount); // 배지 출력
     }
 }
